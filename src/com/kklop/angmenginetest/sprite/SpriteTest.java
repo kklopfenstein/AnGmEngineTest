07dd73ec7e44b88a713e86b3e42eef68bc32d90e
Kevin M. Klopfenstein <kklopfenstein@gmail.com>
2013-02-24 19:46:03 -0500

    updated SpriteTest

--- src/com/kklop/angmenginetest/sprite/SpriteTest.java
+++ src/com/kklop/angmenginetest/sprite/SpriteTest.java
@@ -25,4 +25,18 @@ public class SpriteTest extends AndroidTestCase {
 				com.kklop.angmenginetest.R.drawable.ghost), 23, 23, 60);
 		assertTrue("Sprites have collided", sprite.collided(sprite2));
 	}
+	
+	public void testStaticSpriteUpdate()  throws Exception {
+		Resources res = getContext().getResources();
+		Bitmap bmp = BitmapFactory.decodeResource(res, 
+				com.kklop.angmenginetest.R.drawable.ghost);
+		Log.d(TAG, "Bitmap width is " + bmp.getWidth() + 
+				" and bitmap height is " + bmp.getHeight());
+		assertTrue("Bitmap is valid.", bmp != null && bmp.getWidth() > 0);
+		Sprite sprite = new MockSprite(BitmapFactory.decodeResource(res, 
+				com.kklop.angmenginetest.R.drawable.ghost), 25, 25, 60);
+		Thread.sleep(100);
+		sprite.update(System.currentTimeMillis(), 120, 120, 2, false);
+		assertTrue("Sprite has moved.", sprite.getX() > 25 && sprite.getY() > 25);
+	}
 }
