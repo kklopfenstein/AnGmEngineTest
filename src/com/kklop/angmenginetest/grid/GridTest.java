ad95d5823bd882f95dae222ece36c9c7b8234674
Kevin M. Klopfenstein <kklopfenstein@gmail.com>
2013-03-23 16:59:41 -0400

    Add test for sprite removal from grid.

--- src/com/kklop/angmenginetest/grid/GridTest.java
+++ src/com/kklop/angmenginetest/grid/GridTest.java
@@ -115,4 +115,46 @@ public class GridTest extends AndroidTestCase {
 			assertTrue("Sprite collision isn't istself", !s.equals(sprite));
 		}
 	}
+	
+	public void testGridRemove() throws Exception {
+		Grid grid = new Grid(1800, 900, 90);
+		Resources res = getContext().getResources();
+		Bitmap bmp = BitmapFactory.decodeResource(res, 
+				com.kklop.angmenginetest.R.drawable.ghost);
+		Log.d(TAG, "Bitmap width is " + bmp.getWidth() + 
+				" and bitmap height is " + bmp.getHeight());
+		assertTrue("Bitmap is valid.", bmp != null && bmp.getWidth() > 0);
+		Sprite sprite = new MockSprite(BOUND2, BitmapFactory.decodeResource(res, 
+				com.kklop.angmenginetest.R.drawable.ghost), 25, 25, 60, null);
+		assertTrue("Bot left crnr is not null", sprite.getBotLeftCrnr().x > 0
+				&& sprite.getBotLeftCrnr().y > 0);
+		Sprite sprite2 = new MockSprite(BOUND2, BitmapFactory.decodeResource(res, 
+				com.kklop.angmenginetest.R.drawable.ghost), 900, 800, 60, null);
+		grid.addSprite(sprite);
+		grid.addSprite(sprite2);
+		assertTrue("Should have grid id", sprite.getGridId() == 0);
+		assertTrue("Should have grid id", sprite2.getGridId() == 1);
+		List<Sprite> sprites = grid.getCollisions(sprite);
+		assertTrue("Collided sprites should not be empty and should be one",
+				sprites == null || sprites.size() == 0);
+		Log.d(TAG, "Collisions size " + sprites.size());
+		for(Sprite s: sprites) {
+			assertTrue("Sprite collision isn't istself", !s.equals(sprite));
+		}
+		sprite2.setX(23);
+		sprite2.setY(23);
+		sprite2.setState(Sprite.SPRITE_STATE.MOVING);
+		grid.update();
+		sprites = grid.getCollisions(sprite);
+		assertTrue("Collided sprites should not be empty and should be one",
+				sprites != null && sprites.size() >= 1);
+		Log.d(TAG, "Collisions size " + sprites.size());
+		for(Sprite s: sprites) {
+			assertTrue("Sprite collision isn't istself", !s.equals(sprite));
+		}
+		grid.removeFromGrid(sprite2);
+		sprites = grid.getCollisions(sprite);
+		assertTrue("Collisions should be empty", sprites == null || 
+				sprites.size() == 0);
+	}
 }
